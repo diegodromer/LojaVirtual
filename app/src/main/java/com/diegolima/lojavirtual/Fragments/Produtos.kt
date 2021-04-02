ef1c76396a739ab8753e0b3e45f91af3c5a48549
Diego Lima de Oliveira <diego.dromer@hotmail.com>
2021-04-02 19:03:13 -0300

    alert de pagamento iniciado

--- app/src/main/java/com/diegolima/lojavirtual/Fragments/Produtos.kt
+++ app/src/main/java/com/diegolima/lojavirtual/Fragments/Produtos.kt
@@ -1,11 +1,13 @@
 package com.diegolima.lojavirtual.Fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
+import android.widget.Toast
 import com.diegolima.lojavirtual.Model.Dados
 import com.diegolima.lojavirtual.R
 import com.google.firebase.firestore.FirebaseFirestore
@@ -15,6 +17,7 @@ import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.fragment_produtos.*
 import kotlinx.android.synthetic.main.lista_produtos.view.*
+import kotlinx.android.synthetic.main.pagamento.*
 
 class Produtos : Fragment() {
 
@@ -30,6 +33,27 @@ class Produtos : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Adapter = GroupAdapter()
         recycler_produtos.adapter = Adapter
+
+        Adapter.setOnItemClickListener { item, view ->
+            val DialogView = LayoutInflater.from(context).inflate(R.layout.pagamento, null)
+            val builder = AlertDialog.Builder(context)
+                .setView(DialogView)
+                .setTitle("Formas de pagamento")
+
+            val mAlertDialog = builder.show()
+
+            mAlertDialog.bt_pagar.setOnClickListener {
+                mAlertDialog.dismiss()
+                val pagamento = mAlertDialog.fm_pagamento.text.toString()
+
+                if (pagamento == "249,99") {
+                    Toast.makeText(context, "Pagamento Concluido", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "Pagamento Recusado", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
 
         BuscarProdutos()
 
